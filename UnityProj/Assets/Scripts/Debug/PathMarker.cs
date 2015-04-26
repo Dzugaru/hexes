1476ed2ec4739cdac272e85a4278d97ccd545161
Dzugaru <dzugaru@gmail.com>
2015-04-26 19:04:53 +0300

    gitignore fixed

---
+++ UnityProj/Assets/Scripts/Debug/PathMarker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathMarker : MonoBehaviour, IHasDuration 
+{
+    float dieTime = float.MaxValue;
+
+    public float rotSpeed = 45f;   
+
+	void Update () 
+    {
+        if (Time.time >= dieTime)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, rotSpeed * Time.deltaTime, 0));
+        }
+	}
+
+    public void SetDuration(float dur)
+    {
+        dieTime = Time.time + dur;
+    }
+}
